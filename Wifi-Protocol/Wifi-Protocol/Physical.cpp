95f30e090afbb71d5b440f9fec9cd95e17dbddf6
vincentlau36 <vincentlau36@gmail.com>
2013-11-23 13:55:38 -0800

    Physical.cpp complete for now
    - Added SendControl() function for sending control packets
    - Added SendData() for sending data packets
    - Headers for all Physical.cpp functions

--- Wifi-Protocol/Wifi-Protocol/Physical.cpp
+++ Wifi-Protocol/Wifi-Protocol/Physical.cpp
@@ -21,13 +21,104 @@
 -----------------------------------------------------------------------------*/
 #include "Physical.h"
 
-
-BOOL ReadSerialPort(HANDLE hComm, char* packetBuffer[1024], DWORD dwBytesToRead,
+/*-----------------------------------------------------------------------------
+-	FUNCTION:	ReadSerialPort
+-
+-	DATE:		November 23, 2013
+-
+-	REVISIONS:	...
+-
+-	DESIGNER:	Vincent Lau
+-
+-	PROGRAMMER:	Vincent Lau
+-
+-	INTERFACE:	BOOL ReadSerialPort(HANDLE hComm, char packetBuffer[1024], DWORD dwBytesToRead,
+-					LPDWORD lpdwBytesRead)
+-		
+-	RETURNS:	BOOL - TRUE on Reading sucess, FALSE if reading from serial port failed
+-				
+-	PARAMETERS: HANDLE hComm - Handle to the serial port
+-				char packetBuffer[1024] - the buffer we want to fill and send back
+-				DWORD dwBytesToRead - How many bytes are waiting to be read at the serial port
+-				LPDWORD lpdwBytesRead - how many bytes were read, pass this back in case useful
+-
+-	NOTES:	The physical function to read from the serial port
+-
+-----------------------------------------------------------------------------*/
+BOOL ReadSerialPort(HANDLE hComm, char packetBuffer[1024], DWORD dwBytesToRead,
 					LPDWORD lpdwBytesRead)
 {
-	if (ReadFile(hComm, *packetBuffer, dwBytesToRead, lpdwBytesRead, NULL))
+	if (!ReadFile(hComm, packetBuffer, dwBytesToRead, lpdwBytesRead, NULL))
 	{
-		return TRUE;
+		return FALSE;
 	}
-	return FALSE;
+	return TRUE;
+}
+
+/*-----------------------------------------------------------------------------
+-	FUNCTION:	SendControl
+-
+-	DATE:		November 23, 2013
+-
+-	REVISIONS:	...
+-
+-	DESIGNER:	Vincent Lau
+-
+-	PROGRAMMER:	Vincent Lau
+-
+-	INTERFACE:	BOOL SendControl(HANDLE hComm, int controlType)
+-
+-	RETURNS:	BOOL -	TRUE if writing to the serial port was successful
+-						FALSE if could not write to serial port
+-				
+-	PARAMETERS: HANDLE hComm	- Handle to the serial port
+-				int controlType - Type of control packet to send. (ACK, ENQ, etc.)
+-
+-	NOTES:	This function takes care of sending a simple 2-BYTE control packet
+-			as per the design of the (BE CREATIVE) protocol.
+-			
+-			
+-----------------------------------------------------------------------------*/
+BOOL SendControl(HANDLE hComm, int controlType)
+{
+	DWORD dwBytesSent;
+	char packetToSend[2] = {SYN, controlType};
+	
+	if (!WriteFile(hComm, packetToSend, PACKET_BYTES_CTL, &dwBytesSent, NULL))
+		return FALSE;
+	return TRUE;
+}
+
+/*-----------------------------------------------------------------------------
+-	FUNCTION:	SendData
+-
+-	DATE:		November 23, 2013
+-
+-	REVISIONS:	...
+-
+-	DESIGNER:	Vincent Lau
+-
+-	PROGRAMMER:	Vincent Lau
+-
+-	INTERFACE:	BOOL SendData(HANDLE hComm, char packet[1024])
+-
+-	RETURNS:	BOOL -	TRUE if writing to the serial port was successful
+-						FALSE if could not write to serial port
+-				
+-	PARAMETERS: HANDLE hComm - Handle to the serial port
+-				char packet[1024] - the packet to send, including the 2B header
+-									containing the control chars and 2B trailer
+-									containing the CRC
+-
+-	NOTES:	Sending a data packet formed by a Packetize function. Packet should
+-			already be filled in.
+-
+-----------------------------------------------------------------------------*/
+BOOL SendData(HANDLE hComm, char packetToSend[1024])
+{
+	DWORD dwBytesSent;
+
+	if (!WriteFile(hComm, packetToSend, PACKET_BYTES_TOTAL, &dwBytesSent, NULL))
+		return FALSE;
+	return TRUE;
 }
--- Wifi-Protocol/Wifi-Protocol/Physical.h
+++ Wifi-Protocol/Wifi-Protocol/Physical.h
@@ -1,9 +1,12 @@
 #ifndef PHYSICAL_H
 #define PHYSICAL_H
 #include "Includes.h"
+#include "Vars.h"
+#include "Packet.h"
 
 // Function Prototypes
-BOOL ReadSerialPort(HANDLE, &char[1024], DWORD cbInQue, LPDWORD);
-BOOL WriteToSerialPort(char*);
+BOOL ReadSerialPort(HANDLE, char[1024], DWORD cbInQue, LPDWORD);
+BOOL SendControl(HANDLE, int);
+BOOL SendData (HANDLE, char [1024]);
 
 #endif
