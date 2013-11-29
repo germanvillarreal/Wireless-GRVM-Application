09010ae8cb8210290abb10eb95096e6ad8c1ea2d
roropoh <hsiehrobin@gmail.com>
2013-11-29 12:01:01 -0800

    fixed padding issues in packet

--- Wifi-Protocol/Wifi-Protocol/Packet.cpp
+++ Wifi-Protocol/Wifi-Protocol/Packet.cpp
@@ -15,6 +15,8 @@
 --                  Added PacketCheck function
 --					2013/11/24
 --					Packetize parameter FILE* changed to CHAR*
+--					2013/11/29 - Robin Hsieh
+--					Fixed padding issues.
 --
 --  DESIGNER:       
 --
@@ -36,29 +38,40 @@ CHAR Packet[PACKET_BYTES_TOTAL];
 BOOL Packetize(CHAR* bufferWithFile, int sentPacketCounter)
 {
 	CHAR* data = (CHAR*)malloc(PACKET_BYTES_DATA);
+
 	BOOL isDone = FALSE;
 	size_t fileSize = strlen(bufferWithFile);
 	size_t StartLoc = PACKET_BYTES_DATA * sentPacketCounter; //1020 x sentPacketCounter = startingLocation
-	
+	//free(data);
 	if(StartLoc > fileSize)
 	{
 		fprintf(stderr, "%s", "Cannot seek to this location..");
 		return TRUE; //WE'RE DONE.
 	}
 
+	for (size_t n = 0; n < PACKET_BYTES_TOTAL; n++)
+		Packet[n] = ' ';
+
+
 	for(size_t i = StartLoc, j = 0; j < PACKET_BYTES_DATA; i++, j++)
 	{
-		if(bufferWithFile[i] == '\0' || bufferWithFile[i] == EOF)
+
+		if((bufferWithFile[i] == EOF || bufferWithFile[i] == '\0'))
+			//if(bufferWithFile[i] == '\0' || bufferWithFile[i] == EOF)
+				//if(bufferWithFile[i] == '\0')
 		{
-			data[j] = '\0';
+			data[j] = 'W';
+			//data[j] = '\0';
 			isDone = TRUE;
+			i--;
 		}
 		else
 		{
 			data[j] = bufferWithFile[i];
 		}
 	}
-	
+
+
 	// Add control bytes to the packet
 	Packet[0] = SYN;
 	Packet[1] = (sentPacketCounter % 2 == 0) ? DC1 : DC2;
@@ -66,7 +79,7 @@ BOOL Packetize(CHAR* bufferWithFile, int sentPacketCounter)
 	// Add data bytes to the packet
 	for(size_t i = 0; i < PACKET_BYTES_DATA; i++)
 		Packet[i+2] = data[i];
-	
+
 	// Add the trailer bytes to the packet (CRC)
 	char pktral[2];
 	//char* GenerateCRC(char pkt[GENERATE_CRC_TEST_SIZE], char generatedCRC[2]){ // GENERATE_CRC_TEST_SIZE = 1020
@@ -74,8 +87,10 @@ BOOL Packetize(CHAR* bufferWithFile, int sentPacketCounter)
 	Packet[PACKET_BYTES_TOTAL-2] = pktral[0];
 	Packet[PACKET_BYTES_TOTAL-1] = pktral[1];
 
-	free(data); // Done with the data portion buffer
 
+	char test = data[500];
+	char test2 = data[800];
+	free(data); // Done with the data portion buffer
 	return isDone;
 }
 
@@ -91,27 +106,27 @@ BOOL PacketCheck(HWND hwnd, char packet[1024])
 	//	return FALSE;
 	//}
 
-//HAD TO COMMENT THIS OUT TO TEST COMPILE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//HAD TO COMMENT THIS OUT TO TEST COMPILE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 	switch (packet[1])
 	{
-	//case ENQ:
-	//	bENQReceived = TRUE;
-	//	SendControl(hComm, ACK);
-	//	//Set "what we're waiting for" flag to DC1
-	//	waitForType = DC1;
-	//break;
-	//case ACK:
-	//	// check if we wanted an ACK
-	//	if (waitForType == ACK)
-	//	{
-	//		// check if we're actually bidding for the line
-	//		if (bWantLine)
-	//			ReleaseSemaphore(hWaitForLineSemaphore, 1, NULL);
-	//		else
-	//		// check if we're sending a file
-	//			ReleaseSemaphore(hACKWaitSemaphore, 1, NULL);
-	//	}
-	//break;
+		//case ENQ:
+		//	bENQReceived = TRUE;
+		//	SendControl(hComm, ACK);
+		//	//Set "what we're waiting for" flag to DC1
+		//	waitForType = DC1;
+		//break;
+		//case ACK:
+		//	// check if we wanted an ACK
+		//	if (waitForType == ACK)
+		//	{
+		//		// check if we're actually bidding for the line
+		//		if (bWantLine)
+		//			ReleaseSemaphore(hWaitForLineSemaphore, 1, NULL);
+		//		else
+		//		// check if we're sending a file
+		//			ReleaseSemaphore(hACKWaitSemaphore, 1, NULL);
+		//	}
+		//break;
 
 	case DC1:
 		//if we're waiting for a DC2 packet
@@ -121,57 +136,52 @@ BOOL PacketCheck(HWND hwnd, char packet[1024])
 			SendControl(hComm, NAK);
 			break;
 		}
-	
+
 		SendControl(hComm, ACK);
 		//add to buffer
-//		char p2[1020];
+
 		for(size_t i = 2; i < 1022; i++)
 			p2[i - 2] = packet[i];
 		AddToBuffer(p2);
 
 		DisplayText(hwnd, displayBuffer);
-	break;
-	
-			
+		break;
+
+
 	case DC2:
 		// if we're waiting for a DC1 packet
-		if (waitForType == DC1 || ErrorCheck(packet+2))
+		if (waitForType == DC1 || !ErrorCheck(packet+2))
 		{
 			SendControl(hComm, NAK);
 			break;
 		}
 
-		char p3[1022];
-		for(size_t i = 2; i < 1024; i++)
-			p3[i - 2] = packet[i];
-
 		SendControl(hComm, ACK);
 
 		//add to buffer
-//		char p2[1020];
 		for(size_t i = 2; i < 1022; i++)
 			p2[i - 2] = packet[i];
 		AddToBuffer(p2);
 
 		DisplayText(hwnd, displayBuffer);
-	break;
+		break;
 
 	case NAK:
 		//Set "What we're waiting for" flag to ACK
 		waitForType = ACK;
 		SendData(hComm, Packet); // send the previous packet
-	break;
-		
-	//case EOT:
-	//	bENQReceived = FALSE;
-	//	if (bENQToSend)
-	//	{
-	//		ReleaseSemaphore(hWaitForLineSemaphore, 1, NULL);
-	//		break;
-	//	}
-	//	// GO back to IDLE state
-	//	waitForType = ENQ;
-	//break;
-    }
+		break;
+
+		//case EOT:
+		//	bENQReceived = FALSE;
+		//	if (bENQToSend)
+		//	{
+		//		ReleaseSemaphore(hWaitForLineSemaphore, 1, NULL);
+		//		break;
+		//	}
+		//	// GO back to IDLE state
+		//	waitForType = ENQ;
+		//break;
+	}
 	return TRUE;
 }
